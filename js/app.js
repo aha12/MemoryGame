2685f2088282a575e9b600252d9d990db01b5d14
AidaAbebe <aabebe74@gmail.com>
2018-08-05 15:58:15 -0500

    Added .disable variable to css. .disable has the key-value pair pointer-events:none.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 let card = document.getElementsByClassName('card');
 
-let cards = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb']
+let cards = [...card];
 
-for (var i = 0; i < cards.length; i++) {
+for (let i = 0; i < cards.length; i++) {
 
     cards[i].addEventListener('click', displayCard);
 
+};
+
+function displayCard () {
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+    this.classList.toggle('disable');
 }
+
+
+
+
+
+
+/* 'fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb' */
