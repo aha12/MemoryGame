fb3e42ecddfd7bb46118fa4d361438066adc14c8
AidaAbebe <aabebe74@gmail.com>
2018-08-05 15:32:44 -0500

    Added for loop to iterate over cards in array.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 let card = document.getElementsByClassName('card');
 
 let cards = ['fa fa-diamond', 'fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-bolt', 'fa fa-cube', 'fa fa-cube', 'fa fa-leaf', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bicycle', 'fa fa-bomb', 'fa fa-bomb']
+
+for (var i = 0; i < cards.length; i++) {
+
+    cards[i].addEventListener('click', displayCard);
+
+}
